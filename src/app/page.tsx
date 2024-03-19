6ef66cf80795467efac5b8447349d8a8fc75a4bc
ttthong <tt773972@gmail.com>
2024-03-19 14:39:57 +0700

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,55 @@
-import { Calistoga } from "next/font/google";
-import Image from "next/image";
+import type { NextPage } from "next";
+import InputFieldContainer from "../../components/input-field-container";
+import ProductCategories from "../../components/product-categories";
+import IPhonePro from "../../components/i-phone-pro";
+import ProductImage from "../../components/product-image";
+import ProductAndAccessories from "../../components/product-and-accessories";
+import FrameComponent1 from "../../components/frame-component1";
+import FrameComponent from "../../components/frame-component";
 
-
-export default function Home() {
+const Index1: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="index">
-            <div className="div">
-                <header className="header">
-                    <div className="overlap-group">
-                        <div className="rectangle"/>
-                        <div className="rectangle-2"/>
-                        <div className="text-wrapper">Smartphone Store</div>
-                        <div className="rectangle-3"/>
-                        <div className="text-wrapper-2">Tim kiem san pham</div>
-                        <div className="rectangle-4"/>
-                        <div className="text-wrapper-3">He thong cua hang</div>
-                        <div className="rectangle-5"/>
-                        <div className="rectangle-6"/>
-                        <div className="text-wrapper-4">Tra cuu bao hanh</div>
-                        <div className="rectangle-7"/>
-                        <div className="text-wrapper-5">Gio hang</div>
-                        <div className="rectangle-8"/>
-                        <div className="rectangle-9"/>
-                        <div className="rectangle-10"/>
-                        <div className="rectangle-11"/>
-                        <div className="rectangle-12"/>
-                        <div className="rectangle-13"/>
-                        <div className="rectangle-14"/>
-                        <div className="rectangle-15"/>
-                        <div className="rectangle-16"/>
-                        <div className="rectangle-17"/>
-                        <div className="rectangle-18"/>
-                        <div className="text-wrapper-6">Iphone</div>
-                        <div className="text-wrapper-7">Ipad</div>
-                        <div className="text-wrapper-8">Watch</div>
-                        <div className="text-wrapper-9">Mac</div>
-                        <div className="text-wrapper-10">Am thanh</div>
-                        <div className="text-wrapper-11">Phu kien</div>
-                        <div className="text-wrapper-12">Tra gop</div>
-                        <div className="text-wrapper-13">Thu cu</div>
-                        <div className="text-wrapper-14">Sale</div>
-                        <div className="text-wrapper-15">Tin tuc</div>
-                        <p className="p">Dang nhap/ Dang ky</p>
-                    </div>
-                </header>
-                <div className="overlap">
-                  <div className="rectangle-19" />
-                  <img className="img" alt="Rectangle" src="banner-1.jpg" />
-                  <img className="rectanggle-20" alt="Rectangle" src="banner-2.jpg" />
-                  <div className="rectanggle-21" />
-                  <div className="text-wrapper-16">1900.0091</div>
-                  <div className="rectanggle-22" />
-                  <p className="nang-cap-goi-bao-hanh">
-                    &nbsp;&nbsp; Nang cap &nbsp;&nbsp;goi bao hanh
-                    <br />
-                    Man hinh - Nguon - FaceID
-                  </p>
-                </div>
-                <div className="overlap-2">
-                  <div className="rectanggle-23" />
-                  <div className="rectanggle-24" />
-                  <div className="rectanggle-25" />
-                  <div className="rectanggle-26" />
-                  <div className="text-wrapper-17">Gio lam viec tu 8:30 - 21:00</div>
-                  <div className="text-wrapper-18">Thanh toan Visa, ATM, tin dung,...</div>
-                  <div className="rectanggle-27" />
-                  <div className="text-wrapper-19">Thoi gian doi gia</div>
-                  <div className="text-wrapper-20">Ho tro ky thuat tron doi</div>
-                  <div className="text-wrapper-21">Thanh toan linh hoat</div>
-                  <div className="text-wrapper-22">Giao hang toan quoc</div>
-                  <div className="text-wrapper-23">free ship noi thanh</div>
-                  <div className="text-wrapper-24">Thoi gian tu 7 - 30 ngay doi tra</div>
-                </div>
-            </div>
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[110px] box-border gap-[124px] tracking-[normal] text-left text-mini text-black font-inter mq450:gap-[31px] mq1125:gap-[62px]">
+      <div className="w-[271px] h-[124px] relative bg-darkgray-200 hidden z-[0]" />
+      <i className="w-[77px] h-[18px] relative leading-[18.4px] hidden font-thin [text-shadow:1px_0_0_#000,_0_1px_0_#000,_-1px_0_0_#000,_0_-1px_0_#000] min-w-[77px] z-[1]">
+        IPHONE 15
+      </i>
+      <div className="w-[198px] h-[72px] relative bg-gainsboro hidden z-[2]" />
+      <InputFieldContainer />
+      <div className="self-stretch h-[542px] relative bg-gainsboro hidden z-[4]" />
+      <section className="self-stretch flex flex-col items-start justify-start max-w-full">
+        <ProductCategories />
+        <IPhonePro />
+        <ProductImage />
+        <div className="self-stretch grid flex-row items-start justify-start py-0 pr-1 pl-0 box-border gap-[18.5px] max-w-full grid-cols-[repeat(3,_minmax(422px,_1fr))] mq800:grid-cols-[minmax(422px,_1fr)] mq1350:justify-center mq1350:grid-cols-[repeat(2,_minmax(422px,_732px))]">
+          <div className="h-[526px] flex flex-col items-start justify-start pt-0.5 px-0 pb-0 box-border max-w-full">
+            <img
+              className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full object-cover"
+              loading="lazy"
+              alt=""
+              src="/rectangle-31@2x.png"
+            />
+          </div>
+          <img
+            className="h-[524px] w-[562px] relative max-w-full overflow-hidden max-h-full object-cover min-h-[524px] mq800:w-full"
+            loading="lazy"
+            alt=""
+            src="/rectangle-32@2x.png"
+          />
+          <img
+            className="h-[524px] w-[562px] relative max-w-full overflow-hidden max-h-full object-cover min-h-[524px] mq800:w-full"
+            loading="lazy"
+            alt=""
+            src="/rectangle-33@2x.png"
+          />
         </div>
-    </main>
+        <ProductAndAccessories />
+        <FrameComponent1 />
+      </section>
+      <FrameComponent />
+      <section className="self-stretch h-[709px] relative bg-gainsboro" />
+    </div>
   );
-}
+};
+
+export default Index1;
